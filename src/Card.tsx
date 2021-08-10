0e81025b0b729776b0d275fdb1ce962d1bf6891c
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2021-08-10 21:48:00 +1200

    added style to data displayed

--- src/Card.tsx
+++ src/Card.tsx
@@ -2,14 +2,41 @@ import { cpuUsage } from "process";
 import React, { FC } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Grid, Paper } from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
 import "./Card.css";
+import { classExpression } from "@babel/types";
+import { red } from "@material-ui/core/colors";
 
 type Props = {
   data: string;
   heroName: string;
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper_left: {
+      padding: theme.spacing(2),
+      textAlign: "left",
+      fontWeight: 700,
+      background: "#015BFF",
+      color: "white",
+    },
+    paper_right: {
+      padding: theme.spacing(2),
+      textAlign: "right",
+      fontWeight: 900,
+      background: "#0F2858",
+      color: "white",
+    },
+  })
+);
+
 const Card: React.FC<Props> = (props) => {
+  const classes = useStyles();
   //   console.log(props);
   const keys = Object.keys(props.data);
   const values = Object.values(props.data);
@@ -27,30 +54,32 @@ const Card: React.FC<Props> = (props) => {
   };
   return (
     <div className="block">
-      <Grid container spacing={3}>
-        {stats.map((data) => {
-          const dataPair: String[] = splitData(data);
-          const title: String = dataPair[0];
-          const number: String = dataPair[1];
-          return (
-            <Grid
-              container
-              item
-              sm={12}
-              xs={12}
-              spacing={3}
-              key={Math.random()}
-            >
-              <Grid item sm={8}>
-                <Paper>{title}</Paper>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          {stats.map((data) => {
+            const dataPair: String[] = splitData(data);
+            const title: String = dataPair[0];
+            const number: String = dataPair[1];
+            return (
+              <Grid
+                container
+                item
+                sm={12}
+                xs={12}
+                spacing={3}
+                key={Math.random()}
+              >
+                <Grid item sm={8}>
+                  <Paper className={classes.paper_left}>{title}</Paper>
+                </Grid>
+                <Grid item sm={4}>
+                  <Paper className={classes.paper_right}>{number}</Paper>
+                </Grid>
               </Grid>
-              <Grid item sm={4}>
-                <Paper>{number}</Paper>
-              </Grid>
-            </Grid>
-          );
-        })}
-      </Grid>
+            );
+          })}
+        </Grid>
+      </div>
     </div>
     // <div className="block">
     //   <Container>
